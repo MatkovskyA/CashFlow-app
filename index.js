146d4fc192646396e3ed01befa83711091cd1f17
MatkovskyA <a.matkovsky14@gmail.com>
2023-05-17 12:41:20 +0300

    Добалены категории трат

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const LIMIT = 10000;
 const CURRENCY = 'руб.';
+
+//Объявление переменных строковых + цвет
 const STATUS_IN_LIMIT = 'всё хорошо';
 const STATUS_OUT_OF_LIMIT = 'всё плохо';
 const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_red';
 
-let inputNode = document.querySelector('.js-expenses__input');
-let buttonNode = document.querySelector('.js-expenses__btn');
-let historyNode = document.querySelector('.js-history');
-let sumNode = document.querySelector('.js-sum');
-let limitNode = document.querySelector('.js-limit');
-let statusNode = document.querySelector('.js-status');
-let button2Node = document.querySelector('.js-reset__btn');
+// Объявление ссылок из HTML документа
+const inputNode = document.querySelector('.js-expenses__input');
+const buttonNode = document.querySelector('.js-expenses__btn');
+const categorySelectedNode = document.querySelector('.js-category__select');
+const historyNode = document.querySelector('.js-history');
+const sumNode = document.querySelector('.js-sum');
+const limitNode = document.querySelector('.js-limit');
+const statusNode = document.querySelector('.js-status');
+const button2Node = document.querySelector('.js-reset__btn');
 
-
+// Объявление основной переменной, которая пополняется нажатием кнопки "Добавить"
 let expenses = [];
 
 init(expenses);
 
+// ФУНКЦИИ ------------------------------------------
+// Добавление значения через нажатие на кнопку
 buttonNode.addEventListener('click', function() {
     const consumption = getConsumptionFromUser();
 
@@ -25,11 +31,11 @@ buttonNode.addEventListener('click', function() {
     }
 
     trackConsumption(consumption);
-
     render(expenses);
-
 });
 
+
+// Сброс трат и обнуление истории
 button2Node.addEventListener("click", function () {
     expenses = [];
     sumNode.innerText = expenses;
@@ -59,6 +65,9 @@ function getConsumptionFromUser() {
 
     return consumption;
 };
+
+// Возвращает выбранную пользователем категорию
+
 
 function clearInput() {
     inputNode.value = "";
@@ -101,7 +110,7 @@ function renderStatus(sum) {
         statusNode.innerText = STATUS_IN_LIMIT;
         statusNode.style.color = 'green';
     } else {
-        statusNode.innerText = STATUS_OUT_OF_LIMIT;
+        statusNode.innerText = `${STATUS_OUT_OF_LIMIT} (${LIMIT - sum} руб.)`;
         statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
         statusNode.style.color = 'red';
 
